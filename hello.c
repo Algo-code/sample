6cbcd179ec96a84dcac4c43d3bc3cca28793ead9
Algo-code <eudrickmakiku@yahoo.com>
2019-04-15 21:01:44 +0800

    chage made to hello.c

--- hello.c
+++ hello.c
@@ -5,5 +5,7 @@ int main (){
 	c = (a*a)+(b*b);
 	printf("answer: %d\n", c);
 	printf("am here to save you all");
+	printf("this is a change made to the code ");
+	printf("deal with it");
 return 0;
 } 
